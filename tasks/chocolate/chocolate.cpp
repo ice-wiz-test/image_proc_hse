500ea787aab6342beda93d9d51ad80fd20cd7304
indukvtsiane <prokhoridze16@gmail.com>
2023-09-15 19:56:56 +0300

    Changes to make it work without int64_t

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -1,9 +1,8 @@
 #include "chocolate.h"
 
 bool CanBreakSlices(int height, int width, int slices_amount) {
-    int64_t a = height;
-    a = a * width;
-    if(a <= slices_amount) return false;
+    int required_minimum = slices_amount / width;
+    if(required_minimum >= height) return false;
     if(slices_amount % height == 0) return true;
     if(slices_amount % width == 0) return true;
     return false;
