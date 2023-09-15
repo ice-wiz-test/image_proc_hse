dd8361d7da8562a9abbf02bcb682681176467edd
indukvtsiane <prokhoridze16@gmail.com>
2023-09-16 00:35:22 +0300

    Small attempt to change

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -2,7 +2,12 @@
 
 bool IsLeap(int year) {
     if(year % 400 == 0) return true;
-    if(year % 100 == 0) return false;
-    if(year % 4 == 0) return true;
+    else {
+        if(year % 100 == 0) return false;
+        else {
+            if(year % 4 == 0) return true;
+            else return false;
+        }
+    }
     return false;
 }
