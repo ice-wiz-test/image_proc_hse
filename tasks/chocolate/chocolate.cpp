b68dd9e8f74f33ac3f3d2d3eae5145a18d98ec20
indukvtsiane <prokhoridze16@gmail.com>
2023-09-15 20:15:46 +0300

    Why

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -1,7 +1,7 @@
 #include "chocolate.h"
 
 bool CanBreakSlices(int height, int width, int slices_amount) {
-    if(height <=0 || width <= 0 || slices_amount <= 0) return false;
+    if(height <=0 || width <= 0 || slices_amount < 0) return false;
     if(slices_amount == 0) return false;
     int required_minimum = slices_amount / width;
     if(required_minimum >= height) return false;
