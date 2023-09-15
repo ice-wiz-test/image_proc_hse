11dea22db0e43139214b1663f4b147629b0699dd
indukvtsiane <prokhoridze16@gmail.com>
2023-09-15 19:52:37 +0300

    Small changes

--- tasks/chocolate/chocolate.cpp
+++ tasks/chocolate/chocolate.cpp
@@ -1,5 +1,10 @@
 #include "chocolate.h"
 
 bool CanBreakSlices(int height, int width, int slices_amount) {
-    return true;
+    int64_t a = height;
+    a = a * width;
+    if(a <= slices_amount) return false;
+    if(slices_amount % height == 0) return true;
+    if(slices_amount % width == 0) return true;
+    return false;
 };
