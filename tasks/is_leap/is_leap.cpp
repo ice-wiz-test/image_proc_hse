55367e7ca50e436cc426fb300b14d7873e8ac05f
indukvtsiane <prokhoridze16@gmail.com>
2023-09-16 00:41:48 +0300

    Logical change

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,7 +1,7 @@
 #include "is_leap.h"
 
 bool IsLeap(int year) {
-    if(year < 0) year = -year - 3;
+    if(year < 0) year = -year - 4;
     if(year % 400 == 0) return true;
     else {
         if(year % 100 == 0) return false;
