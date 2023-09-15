9124de16205254bf31b95d379dcdeed5653e453b
indukvtsiane <prokhoridze16@gmail.com>
2023-09-16 00:39:36 +0300

    Negative numbers

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,6 +1,7 @@
 #include "is_leap.h"
 
 bool IsLeap(int year) {
+    if(year < 0) year = -year - 3;
     if(year % 400 == 0) return true;
     else {
         if(year % 100 == 0) return false;
@@ -9,5 +10,4 @@ bool IsLeap(int year) {
             else return false;
         }
     }
-    return false;
 }
