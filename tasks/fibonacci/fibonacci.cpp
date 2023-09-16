c7b5665471059d7989efe333c592ca488abc6882
indukvtsiane <prokhoridze16@gmail.com>
2023-09-16 19:12:29 +0300

    Readablitiy changes

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,11 +1,15 @@
 #include "fibonacci.h"
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
     int64_t current_val = 1;
     int64_t prev_val = 0;
-    for(int count = 2; count <= n; ++count) {
+    for (int count = 2; count <= n; ++count) {
         int next_member = current_val + prev_val;
         prev_val = current_val;
         current_val = next_member;
