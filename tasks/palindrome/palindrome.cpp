5f9b454e7732a79a0346f895fa636ec98e63969f
indukvtsiane <prokhoridze16@gmail.com>
2023-09-17 00:24:00 +0300

    Comment changes + brace added

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,10 +1,12 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    if (str.empty()) return true;
+    if (str.empty()) {
+        return true;
+    }
     size_t front_iterator = 0;
     size_t last_iterator = str.size();
-    last_iterator--; // не переполняется, так как str.size() >= 1
+    last_iterator--;  // не переполняется, так как str.size() >= 1
     while (front_iterator < last_iterator) {
         while (front_iterator < last_iterator && str[front_iterator] == ' ') {
             front_iterator++;
