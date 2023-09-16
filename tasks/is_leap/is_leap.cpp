bd6a6f85d0a9b3bb914662d5bc3251ae2f0b9ab8
indukvtsiane <prokhoridze16@gmail.com>
2023-09-16 17:16:29 +0300

    Linter changes

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -4,13 +4,13 @@ const int kMostImportantConstant = 400;
 const int kUsualConstant = 4;
 const int kCancelConstant = 100;
 bool IsLeap(int year) {
-    if (year % kMostImportantConstant == 0) 
+    if (year % kMostImportantConstant == 0)
         return true;
     else {
         if (year % kCancelConstant == 0)
             return false;
         else {
-            if(year % kUsualConstant == 0) 
+            if (year % kUsualConstant == 0)
                 return true;
             else {
                 return false;
