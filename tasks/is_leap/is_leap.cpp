1932774bda450f8494340f45b467f657cb126ee7
indukvtsiane <prokhoridze16@gmail.com>
2023-09-16 18:31:12 +0300

    Added space + small changes

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -3,18 +3,16 @@
 const int K_MOST_IMPORTANT_CONSTANT = 400;
 const int K_USUAL_CONSTANT = 4;
 const int K_CANCEL_CONSTANT = 100;
+
 bool IsLeap(int year) {
     if (year % K_MOST_IMPORTANT_CONSTANT == 0) {
         return true;
-    } else {
-        if (year % K_CANCEL_CONSTANT == 0) {
-            return false;
-        } else {
-            if (year % K_USUAL_CONSTANT == 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
     }
+    if (year % K_CANCEL_CONSTANT == 0) {
+            return false;
+    }
+    if (year % K_USUAL_CONSTANT == 0) {
+        return true;
+    }
+    return false;
 }
