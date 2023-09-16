f49e7321d426fa1bbd5413a2a815428a981c9ffe
indukvtsiane <prokhoridze16@gmail.com>
2023-09-16 23:22:40 +0300

    Added a space

--- tasks/min_divisor/min_divisor.cpp
+++ tasks/min_divisor/min_divisor.cpp
@@ -5,7 +5,7 @@ int64_t MinDivisor(int64_t number) {
         return 1;
     }
     int64_t start_value = number;
-    for(int64_t current_checking = 2; current_checking * current_checking <= number; ++current_checking) {
+    for (int64_t current_checking = 2; current_checking * current_checking <= number; ++current_checking) {
         if (number % current_checking == 0) {
             return current_checking;
         }
