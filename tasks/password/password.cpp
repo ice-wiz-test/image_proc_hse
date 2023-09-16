3d83fab59d0b374d3be887095683b3ef22a165ea
indukvtsiane <prokhoridze16@gmail.com>
2023-09-17 00:44:43 +0300

    Global variables readability changes

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -6,10 +6,10 @@ const char K_BIG_MINIMUM_LETTER = 'A';
 const char K_BIG_MAXIMUM_LETTER = 'Z';
 const char K_MINIMUM_DIGIT = '0';
 const char K_MAXIMUM_DIGIT = '9';
-const size_t minimum_size = 8;
-const size_t maximum_size = 14;
-const int minimum_code = 33;
-const int maximum_code = 126;
+const size_t MINIMUM_SIZE = 8;
+const size_t MAXIMUM_SIZE = 14;
+const int MINIMUM_CODE = 33;
+const int MAXIMUM_CODE = 126;
 
 bool IsSmallLetter(char a) {
     if (K_MINIMUM_LETTER <= a && a <= K_MAXIMUM_LETTER) {
@@ -33,7 +33,7 @@ bool IsDigit(char a) {
 }
 
 bool ValidatePassword(const std::string& password) {
-    if (password.size() < minimum_size || password.size() > maximum_size) {
+    if (password.size() < MINIMUM_SIZE || password.size() > MAXIMUM_SIZE) {
         return false;
     }
     size_t count_small_letters = 0;
@@ -42,7 +42,7 @@ bool ValidatePassword(const std::string& password) {
     size_t count_other = 0;
     for (size_t iter = 0; iter < password.size(); ++iter) {
         int code = static_cast<int>(password[iter]);
-        if (code < minimum_code || code > maximum_code) {
+        if (code < MINIMUM_CODE || code > MAXIMUM_CODE) {
             return false;
         }
         if (IsSmallLetter(password[iter])) {
