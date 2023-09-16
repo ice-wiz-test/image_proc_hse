3c5df10f798ff8e32cfae355e6ea895280d12bf5
indukvtsiane <prokhoridze16@gmail.com>
2023-09-16 19:06:46 +0300

    Added counting.

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,5 +1,13 @@
 #include "fibonacci.h"
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    return 0;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    int current_val = 1;
+    int prev_val = 0;
+    for(int count = 2; count <= n; ++count) {
+        std::swap(current_val, prev_val);
+        current_val += prev_val;
+    }
+    return current_val;
 }
