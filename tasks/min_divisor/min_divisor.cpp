a7e07d6a763a31c2ff0b6316a1d59b69afcb855d
indukvtsiane <prokhoridze16@gmail.com>
2023-09-16 23:20:59 +0300

    Iterating to square

--- tasks/min_divisor/min_divisor.cpp
+++ tasks/min_divisor/min_divisor.cpp
@@ -1,5 +1,14 @@
 #include "min_divisor.h"
 
 int64_t MinDivisor(int64_t number) {
-    return 2;
+    if (number == 1) {
+        return 1;
+    }
+    int64_t start_value = number;
+    for(int64_t current_checking = 2; current_checking * current_checking <= number; ++current_checking) {
+        if (number % current_checking == 0) {
+            return current_checking;
+        }
+    }
+    return start_value;
 }
