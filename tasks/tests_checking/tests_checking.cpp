edf4c2364f666934711add4114e7d7af45a77b03
Cheaplifer <larichev.po@students.sch2.ru>
2023-09-28 12:11:53 +0300

    Naming changes

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -2,19 +2,19 @@
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    std::deque<std::string> currentOrder;
+    std::deque<std::string> current_order;
     for (const StudentAction& s1 : student_actions) {
         if (s1.side == Side::Top) {
-            currentOrder.push_front(s1.name);
+            current_order.push_front(s1.name);
         } else {
-            currentOrder.push_back(s1.name);
+            current_order.push_back(s1.name);
         }
     }
-    std::vector<std::string> returnOrder;
-    returnOrder.reserve(queries.size());
+    std::vector<std::string> return_order;
+    return_order.reserve(queries.size());
     for (size_t num : queries) {
         num--;  // >= 1
-        returnOrder.push_back(currentOrder[num]);
+        return_order.push_back(current_order[num]);
     }
-    return returnOrder;
+    return return_order;
 }
