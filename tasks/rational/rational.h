acf1f6faa73e2630bca6c9c2472f42478acd29c8
Cheaplifer <larichev.po@students.sch2.ru>
2023-10-09 23:52:52 +0300

    First commit

--- tasks/rational/rational.h
+++ tasks/rational/rational.h
@@ -12,67 +12,179 @@ public:
 
 class Rational {
 public:
-    Rational();
+    Rational() {
+        Set(static_cast<int64_t>(1), static_cast<int64_t>(1));
+    }
 
-    Rational(int value);  // NOLINT
+    Rational(int value) { // NOLINT
+        Set(value, static_cast<int64_t>(1)); // NOLINT
+    }  // NOLINT
 
-    Rational(int numer, int denom);
+    Rational(int numer, int denom) {
+        Set(numer, denom);
+    }
 
-    int GetNumerator() const;
+    int GetNumerator() const {
+        return numer_;
+    }
 
-    int GetDenominator() const;
+    int GetDenominator() const {
+        return denom_;
+    }
 
-    void SetNumerator(int value);
+    void SetNumerator(int value) {
+        Set(value, denom_);
+    }
 
-    void SetDenominator(int value);
+    void SetDenominator(int value) {
+        Set(numer_, value);
+    }
 
-    friend Rational& operator+=(Rational& lhs, const Rational& rhs);
+    friend Rational& operator+=(Rational& lhs, const Rational& rhs) {
+        int64_t new_denom = std::lcm(lhs.denom_, rhs.denom_);
+        int64_t multiply_by_first_ratio = new_denom / lhs.denom_;
+        int64_t multiply_by_second_ratio = new_denom / rhs.denom_;
+        int new_numer = lhs.numer_ * multiply_by_first_ratio + rhs.numer_ * multiply_by_second_ratio;
+        lhs.Set(new_numer, new_denom);
+        return lhs;
+    }
 
-    friend Rational& operator*=(Rational& lhs, const Rational& rhs);
+    friend Rational& operator*=(Rational& lhs, const Rational& rhs) {
+        lhs.Set(lhs.numer_ * rhs.numer_, lhs.denom_ * rhs.denom_);
+        return lhs;
+    }
 
-    friend Rational& operator++(Rational& ratio);  // faster than += 1
+    friend Rational& operator++(Rational& ratio) {
+        ratio.numer_ += ratio.denom_;
+        if (ratio.numer_ % ratio.denom_ == 0) {
+            ratio.numer_ /= ratio.denom_;
+            ratio.denom_ = 1;
+        }
+        return ratio;
+    }  // faster than += 1
 
-    friend Rational& operator--(Rational& ratio);
+    friend Rational& operator--(Rational& ratio) {
+        ratio.numer_ -= ratio.denom_;
+        if (ratio.numer_ % ratio.denom_ == 0) {
+            ratio.numer_ /= ratio.denom_;
+            ratio.denom_ = 1;
+        }
+        return ratio;
+    }
 
-    friend std::istream& operator>>(std::istream& is, Rational& ratio);
+    friend std::istream& operator>>(std::istream& is, Rational& ratio) {
+        std::string s1 = ""; is >> s1;
+        std::string numer; std::string denom;
+        bool have_found_slash = false;
+        for(size_t i = static_cast<size_t>(0); i < s1.size(); ++i) {
+            if (s1[i] == '/') {
+                have_found_slash = true;
+                break;
+            }
+            if (!have_found_slash) numer.push_back(s1[i]);
+            else denom.push_back(s1[i]);
+        }
+        ratio.Set(std::stoll(numer), std::stoll(denom));
+        return is;
+    };
 
 private:
-    void Set(int64_t numer, int64_t denom);
+    void Set(int64_t numer, int64_t denom) {
+        if (denom == 0) {
+            throw RationalDivisionByZero{};
+        }
+        int GCD = std::gcd(numer, denom);
+        numer_ = numer / GCD;
+        denom_ = denom / GCD;
+        if (denom < 0) {
+            numer_ *= -1;
+            denom_ *= -1;
+        }
+    }
 
     int numer_;
     int denom_;
 };
 
-Rational operator+(const Rational& ratio);
+Rational operator+(const Rational& ratio) {
+    return Rational(ratio.GetNumerator(), ratio.GetDenominator());
+};
 
-Rational operator-(const Rational& ratio);
+Rational operator-(const Rational& ratio) {
+    return Rational(-ratio.GetNumerator(), ratio.GetDenominator());
+};
 
-Rational& operator-=(Rational& lhs, const Rational& rhs);
+Rational& operator-=(Rational& lhs, const Rational& rhs) {
+    Rational ret = Rational(lhs.GetNumerator(), lhs.GetDenominator());
+    ret += -rhs;
+    return lhs;
+};
 
-Rational& operator/=(Rational& lhs, const Rational& rhs);
+Rational& operator/=(Rational& lhs, const Rational& rhs) {
+    Rational ret = Rational(lhs.GetNumerator(), lhs.GetDenominator());
+    Rational flp = Rational(rhs.GetDenominator(), rhs.GetNumerator());
+    ret *= flp;
+    return lhs;
+};
 
-Rational operator+(const Rational& lhs, const Rational& rhs);
+Rational operator+(const Rational& lhs, const Rational& rhs) {
+    int64_t new_denom = std::lcm(lhs.GetDenominator(), rhs.GetDenominator());
+    int64_t multiply_by_first_ratio = new_denom / lhs.GetDenominator();
+    int64_t multiply_by_second_ratio = new_denom / rhs.GetDenominator();
+    return Rational(lhs.GetNumerator() * multiply_by_first_ratio+ + rhs.GetNumerator() * multiply_by_second_ratio, new_denom);
+};
 
-Rational operator-(const Rational& lhs, const Rational& rhs);
+Rational operator-(const Rational& lhs, const Rational& rhs) {
+    return lhs + -rhs;
+};
 
-Rational operator*(const Rational& lhs, const Rational& rhs);
+Rational operator*(const Rational& lhs, const Rational& rhs) {
+    Rational ret = Rational(lhs.GetNumerator(), lhs.GetDenominator());
+    ret *= rhs;
+    return ret;
+};
 
-Rational operator/(const Rational& lhs, const Rational& rhs);
+Rational operator/(const Rational& lhs, const Rational& rhs) {
+    Rational ret = Rational(lhs.GetNumerator(), lhs.GetDenominator());
+    ret /= rhs;
+    return ret;
+};
 
-Rational operator++(Rational& ratio, int);
+Rational operator++(Rational& ratio, int) {
+    ratio.SetNumerator(ratio.GetNumerator() + ratio.GetDenominator());
+    return Rational(ratio.GetNumerator(), ratio.GetDenominator());
+};
 
-Rational operator--(Rational& ratio, int);
+Rational operator--(Rational& ratio, int) {
+    ratio.SetNumerator(ratio.GetNumerator() - ratio.GetDenominator());
+    return Rational(ratio.GetNumerator(), ratio.GetDenominator());
+};
 
-bool operator<(const Rational& lhs, const Rational& rhs);
+bool operator<(const Rational& lhs, const Rational& rhs) {
+    return lhs.GetNumerator() * rhs.GetDenominator() < lhs.GetDenominator() * rhs.GetNumerator();
+};
 
-bool operator>(const Rational& lhs, const Rational& rhs);
+bool operator>(const Rational& lhs, const Rational& rhs) {
+    return lhs.GetNumerator() * rhs.GetDenominator() > lhs.GetDenominator() * rhs.GetNumerator();
+};
 
-bool operator<=(const Rational& lhs, const Rational& rhs);
+bool operator<=(const Rational& lhs, const Rational& rhs) {
+    return lhs.GetNumerator() * rhs.GetDenominator() <= lhs.GetDenominator() * rhs.GetNumerator();
+};
 
-bool operator>=(const Rational& lhs, const Rational& rhs);
+bool operator>=(const Rational& lhs, const Rational& rhs) {
+    return lhs.GetNumerator() * rhs.GetDenominator() >= lhs.GetDenominator() * rhs.GetNumerator();
+};
 
-bool operator==(const Rational& lhs, const Rational& rhs);
+bool operator==(const Rational& lhs, const Rational& rhs) {
+    return lhs.GetNumerator() == rhs.GetNumerator() && lhs.GetDenominator() == rhs.GetDenominator();
+};
 
-bool operator!=(const Rational& lhs, const Rational& rhs);
+bool operator!=(const Rational& lhs, const Rational& rhs) {
+    return lhs.GetNumerator() != rhs.GetNumerator() || lhs.GetDenominator() != rhs.GetDenominator();
+};
 
-std::ostream& operator<<(std::ostream& os, const Rational& ratio);
+std::ostream& operator<<(std::ostream& os, const Rational& ratio) {
+    os << ratio.GetNumerator() << "/" << ratio.GetDenominator();
+    return os;
+};
