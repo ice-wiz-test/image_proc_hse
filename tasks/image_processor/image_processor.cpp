cf49a2f375fb3337adc25ef5e897ab012e278c0f
Cheaplifer <larichev.po@students.sch2.ru>
2023-10-29 01:30:07 +0300

    Linter fighting

--- tasks/image_processor/image_processor.cpp
+++ tasks/image_processor/image_processor.cpp
@@ -12,29 +12,27 @@ int main(int argc, char** argv) {
     BMP write_BMP;
     try {
         write_BMP = BMP(file_name.c_str());
-    }
-    catch(std::exception& e) {
+    } catch(std::exception& e) {
         std::cerr << "Try again\n" << std::endl;
         return 0;
     }
     std::string write_to_name = argv[2];
     Controller current_controller = Controller();
-    for(int j = 3; j < argc; ++j) {
+    for (int j = 3; j < argc; ++j) {
         std::string current_argument = argv[j];
         current_controller.add_string(current_argument);
     }
     current_controller.make_all_adjustments(write_BMP);
     try {
         write_BMP.write(write_to_name.c_str());
-    }
-    catch(std::exception& e) {
+    } catch(std::exception& e) {
         std::cerr << "Try again\n" << std::endl;
         return 0;
     }
-   /*BMP write_BMP = BMP("C:/Windows/System32/2023-cpp-pmi-pilot/pmi-233-1-Prokhor-Larichev-indukvtsiane/tasks/image_processor/lenna.bmp");
-   Controller generic_controller = Controller();
-   generic_controller.add_string("-gs");
-   generic_controller.add_string("-neg");
-   generic_controller.make_all_adjustments(write_BMP);
-   write_BMP.write("C:/Windows/System32/2023-cpp-pmi-pilot/pmi-233-1-Prokhor-Larichev-indukvtsiane/tasks/image_processor/test2.bmp");*/
+    /*BMP write_BMP = BMP("C:/Windows/System32/2023-cpp-pmi-pilot/pmi-233-1-Prokhor-Larichev-indukvtsiane/tasks/image_processor/lenna.bmp");
+    Controller generic_controller = Controller();
+    generic_controller.add_string("-gs");
+    generic_controller.add_string("-neg");
+    generic_controller.make_all_adjustments(write_BMP);
+    write_BMP.write("C:/Windows/System32/2023-cpp-pmi-pilot/pmi-233-1-Prokhor-Larichev-indukvtsiane/tasks/image_processor/test2.bmp");*/
 }
