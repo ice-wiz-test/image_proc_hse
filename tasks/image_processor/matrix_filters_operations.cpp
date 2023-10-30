bb274dc73bf03375fbb0fc5a0c28668e686ec916
Cheaplifer <larichev.po@students.sch2.ru>
2023-10-30 17:31:33 +0300

    ads

--- tasks/image_processor/matrix_filters_operations.cpp
+++ tasks/image_processor/matrix_filters_operations.cpp
@@ -125,7 +125,8 @@ void GaussianFilter::Process(BMP& image) {
             double new_r = 0;
             double new_b = 0;
             double new_g = 0;
-            for (int32_t small_x = std::max(0, x0 - MAX_CONSTANT); small_x < std::min(image.bmp_info_header.height, x0 + MAX_CONSTANT); ++small_x) {
+            for (int32_t small_x = std::max(0, x0 - MAX_CONSTANT);
+                 small_x < std::min(image.bmp_info_header.height, x0 + MAX_CONSTANT); ++small_x) {
                 Pixel* pref = image.At(small_x, y0);
                 double sqr = static_cast<double>((small_x - x0) * (small_x - x0));
                 double mult_coeff = exp(-(sqr) / DIVIDE_POWER);
@@ -148,7 +149,8 @@ void GaussianFilter::Process(BMP& image) {
             double new_r = 0;
             double new_b = 0;
             double new_g = 0;
-            for (int32_t small_y = std::max(0, y0 - MAX_CONSTANT); small_y < std::min(image.bmp_info_header.width, y0 + MAX_CONSTANT); ++small_y) {
+            for (int32_t small_y = std::max(0, y0 - MAX_CONSTANT);
+                 small_y < std::min(image.bmp_info_header.width, y0 + MAX_CONSTANT); ++small_y) {
                 Pixel* pref = image.At(x0, small_y);
                 double sqr = static_cast<double>((small_y - y0) * (small_y - y0));
                 double mult_coeff = exp(-(sqr) / DIVIDE_POWER);
